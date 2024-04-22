041729ea8c03bd2ed3b9d3f7b29900eec47e113e
Aiman Fadillah <mlakece914@gmail.com>
2024-04-22 21:44:38 +0700

    create server side

--- src/app/barang/page.js
+++ src/app/barang/page.js
@@ -1,17 +1,14 @@
 "use client"
 
-import { useRouter } from "next/navigation"
+import { redirect, useRouter } from "next/navigation"
 
 export default function Page() {
-    const router = useRouter()
-
     return <div className="container mt-5">
         <div className="row justify-content-center">
             <div className="col-8">
-                <form onSubmit={async (e) => {
-                    e.preventDefault()
-                    await fetch("/api/barang",{method:"POST",body:new FormData(e.target)})
-                    router.push("/")
+                <form action={async (formData) => {
+                    await fetch("/api/barang",{method:"POST",body:formData})
+                    redirect("/")
                 }} className="border rounded p-3">
                     <h1 className="text-center" >Buat Barang</h1>
                     <div className="mt-4" >
